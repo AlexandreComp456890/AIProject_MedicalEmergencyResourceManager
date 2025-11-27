7251b94c3f30a9201dd0fc257695bab0b8ae90c1
AlexandreComp456890 <alexandre.rocha.fonte@gmail.com>
2025-11-26 23:48:12 -0300

    Modificado geo: 	- Função heurística e custo movidas dos algoritmos 	de busca e implementadas nas utilidades.

--- Model/Utils/geo.py
+++ Model/Utils/geo.py
@@ -12,3 +12,21 @@ def haversine(lat1, lon1, lat2, lon2):
          math.sin(dlon/2)**2)
 
     return 2 * R * math.asin(math.sqrt(a))
+
+def cost_to_move(graph, from_node, to_node) -> float:
+        edge = graph.get_edge(from_node, to_node)
+        
+        if edge:
+            #Velocidade em m/s
+            vmps = edge.speed_limit / 3.6
+            time_cost = (edge.distance / vmps) * edge.traffic_factor
+            return time_cost 
+        return math.inf
+    
+def heuristic(graph, node_a, node_b) -> float:
+     edge = graph.get_edge(node_a, node_b)
+     
+     if edge:
+          heuristic_cost = edge.distance / (edge.speed_limit / 3.6) * edge.traffic_factor
+          return heuristic_cost
+     return math.inf
